25113deade789b2d7eb026b4a46d869c7074644d
Sudhanshu-WMW <sudhanshu250403@gmail.com>
2024-05-14 10:17:35 +0530

    random question fetch functionality added

--- app/api/question/route.js
+++ app/api/question/route.js
@@ -5,49 +5,16 @@ import dbConnect from '@/app/middleware/database';
 export async function GET() {
   dbConnect();
   try{
-    let question = await Question.find();
+    const pipeline = [
+      { $sample: { size: 5} }
+    ];
+    let question = await Question.aggregate(pipeline);
     return NextResponse.json({question}, {status: 200})
   }
   catch(error){
     return NextResponse.json({error}, {status: 400})
   }
 }
-// export async function POST(req, res) {
-//   // Ensure that the database is connected
-//   await dbConnect();
-
-//   if (req.method === 'POST') {
-//     try {
-//       // Parse the JSON request body
-//       const requestData = await req.json();
-
-//       // Extract data from the parsed request body
-//       const { question, answer, options, unit, topic, subject } = requestData;
-
-//       // Create a new question object
-//       const newQuestion = new Question({
-//         question,
-//         answer,
-//         options,
-//         unit,
-//         topic,
-//         subject
-//       });
-
-//       // Save the new question to the database
-//       await newQuestion.save();
-//       console.log(newQuestion)
-//       // Return a success message in the response
-//       return NextResponse.json({ message: 'Question added successfully' },{status: 201});
-//     } catch (error) {
-//       // If an error occurs, return the error in the response
-//       return NextResponse.json({ error: error.message },{status: 400});
-//     }
-//   } else {
-//     // If the request method is not POST, return a method not allowed error
-//     return NextResponse.json({ message: 'Method Not Allowed' },{status: 500});
-//   }
-// }
 export async function POST(req, res) {
   // Ensure that the database is connected
   await dbConnect();
