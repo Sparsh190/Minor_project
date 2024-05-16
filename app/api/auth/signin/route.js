20b44ed6f0b7c633cec794af69dc6bc021a55b2c
Sudhanshu-WMW <sudhanshu250403@gmail.com>
2024-05-16 23:14:43 +0530

    sign sign up ADDED

--- app/api/auth/signin/route.js
+++ app/api/auth/signin/route.js
@@ -16,17 +16,17 @@ export async function POST(req, res) {
       });
 
       if (!user) {
-        return NextResponse.json({ error: 'User not found' }, { status: 404 });
+        return NextResponse.json({success:false}, { status: 200 });
       }
 
       // Check if the password is correct
       if (user.password !== password) {
-        return NextResponse.json({ error: 'Incorrect password' }, { status: 401 });
+        return NextResponse.json({ success: false }, { status: 401 });
       }
 
-      return NextResponse.json({ message: 'User signed in successfully' }, { status: 200 });
+      return NextResponse.json({ success:true }, { status: 200 });
     } catch (error) {
-      return NextResponse.json({ error: error.message }, { status: 400 });
+      return NextResponse.json({ success:false }, { status: 400 });
     }
   } else {
     return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
--- app/auth/signUp/page.tsx
+++ app/auth/signUp/page.tsx
@@ -1,10 +1,16 @@
 "use client";
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { Label } from "@/app/components/ui/label";
 import { Input } from "@/app/components/ui/input";
 import { cn } from "@/assets/utils/cn";
-
+import { useRouter } from "next/navigation";
 export default function page() {
+  const router = useRouter();
+  useEffect(() => {
+    if(localStorage.getItem('username')){
+        router.push('/generateQuiz');
+    }
+}, [])
     return (
       <div className="w-full mx-auto flex justify-center items-center h-screen z-10 bg-contain bg-no-repeat bg-opacity-50" style={{background:`url('../quiz_bg.jpg')`}}>
       <SignUpForm />
@@ -13,9 +19,37 @@ export default function page() {
 }
 
 const SignUpForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [firstname, setFirstname] = useState("");
+
+  const router = useRouter();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Form submitted");
+    const signIn = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/auth/signup', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ fullname:firstname, username: username,email: email,password: password }),
+      });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const success = await response.json();
+        if(success.success){
+          router.push('/auth/signIn');
+        }else{
+          alert("User already exists with this email or username")
+        }
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
+    };
+    signIn();
   };
   return (
     <div className="max-w-md w-full mx-auto border border-white rounded-2xl p-8 shadow-input bg-black bg-opacity-80 backdrop-blur-sm">
@@ -29,25 +63,21 @@ const SignUpForm = () => {
       <form className="my-8" onSubmit={handleSubmit}>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
-            <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" />
-          </LabelInputContainer>
-          <LabelInputContainer>
-            <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Durden" type="text" />
+            <Label htmlFor="firstname">Full name</Label>
+            <Input id="firstname" placeholder="Tyler" defaultValue={firstname} onChange={(e)=>setFirstname(e.target.value)}  type="text" />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="username">Username</Label>
-          <Input id="username" placeholder="tyler_durden" type="text" />
+          <Input id="username" placeholder="tyler_durden" defaultValue={username} onChange={(e)=>setUsername(e.target.value)} type="text" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="tylerdurden@gmail.com" type="email" />
+          <Input id="email" placeholder="tylerdurden@gmail.com" defaultValue={email} onChange={(e)=>setEmail(e.target.value)} type="email" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+          <Input id="password" placeholder="••••••••" defaultValue={password} onChange={(e)=>setPassword(e.target.value)} type="password" />
         </LabelInputContainer>
 
         <button
